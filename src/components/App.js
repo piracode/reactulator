9cb96bb708859d311fe351cc5b40624564e0be7e
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-04-19 14:49:43 -0700

    more stuff

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,35 @@ import Display from "./Display";
 import Button from "./Button";
 
 export default function App() {
-  const [displayValue, setDisplayValue] = useState("");
-  // const [currentValue, setCurrentValue] = useState("");
+  const [displayValue, setDisplayValue] = useState("0");
   const [previousOperator, setPreviousOperator] = useState("");
   const [previousValue, setPreviousValue] = useState("");
+  const [secondOperand, setSecondOperand] = useState("");
+  const [firstOperand, setFirstOperand] = useState("");
+  const [selectedOperator, setSelectedOperator] = useState("");
+
+  function calculate() {
+    switch (previousOperator) {
+      case "add":
+        return parseFloat(previousValue) + parseFloat(displayValue);
+      case "subtract":
+        return parseFloat(previousValue) - parseFloat(displayValue);
+      case "multiply":
+        return parseFloat(previousValue) * parseFloat(displayValue);
+      case "divide":
+        return parseFloat(previousValue) / parseFloat(displayValue);
+      default:
+        return parseFloat(displayValue);
+    }
+  }
 
   function handleButtonClick(button) {
     // console.log("Button was clicked!", button);
     switch (button.type) {
-      case "number":
-        // Append the number to the displayValue
-        setDisplayValue((prevDisplayValue) => prevDisplayValue + button.text);
-        break;
-
       case "clear":
         if (button.text === "AC") {
           // Clear everything
           setDisplayValue("");
-          setPreviousValue("");
-          setPreviousOperator("");
-          // setCurrentValue("");
         } else if (button.text === "C") {
           // Remove the last character from the displayValue
           setDisplayValue((prevDisplayValue) =>
@@ -32,37 +41,51 @@ export default function App() {
           );
         }
         break;
-
-      case "operator":
-        // If no previous operator has been chosen, set the displayValue as the previousValue. The previous value is the first ioperand of the operation and the curretn display is the second operand.
-        if (previousOperator === "") {
-          setPreviousValue(displayValue);
+      case "number":
+        // If there is no operator selected yet, add the clicked number to the display value
+        if (selectedOperator === "") {
+          console.log(
+            `line 48 previous value: ${previousValue}/ previousOperator: ${previousOperator}`
+          );
+          setDisplayValue((prevDisplayValue) =>
+            prevDisplayValue === "0"
+              ? button.text
+              : prevDisplayValue + button.text
+          );
         } else {
-          switch (previousOperator) {
-            case "add":
-              setPreviousValue(
-                (prevValue) => prevValue + parseFloat(displayValue)
-              );
-              break;
-            case "subtract":
-              setPreviousValue(
-                (prevValue) => prevValue - parseFloat(displayValue)
-              );
-              break;
-            case "multiply":
-              setPreviousValue(
-                (prevValue) => prevValue * parseFloat(displayValue)
-              );
-              break;
-            case "divide":
-              setPreviousValue(
-                (prevValue) => prevValue / parseFloat(displayValue)
-              );
-              break;
-            default:
-              break;
-          }
+          // If there is an operator selected, update the second operand with the clicked number
+          console.log(
+            `line 58 previous value' + ${previousValue}/ previousOperator: ${previousOperator}`
+          );
+          setSecondOperand(
+            (prevSecondOperand) => prevSecondOperand + button.text
+          );
+          console.log(
+            `line 64 previous value' + ${previousValue}/ previousOperator: ${previousOperator}`
+          );
+          setDisplayValue((prevDisplayValue) =>
+            prevDisplayValue === "0"
+              ? button.text
+              : prevDisplayValue + button.text
+          );
+          console.log(
+            `previous value' + ${previousValue}, previousOperator+ ${previousOperator}`
+          );
         }
+        break;
+      case "operator":
+        if (selectedOperator === "") {
+          setSelectedOperator(button.value);
+          setFirstOperand(parseFloat(displayValue));
+          setDisplayValue("");
+        } else {
+          const result = calculate();
+          setSelectedOperator(button.value);
+          setFirstOperand(result);
+          setSecondOperand("");
+          setDisplayValue("");
+        }
+        break;
     }
   }
 
