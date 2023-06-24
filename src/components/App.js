421437e66610ea6223b300411a4112b6c832625d
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-06-24 13:42:14 -0700

    added the basename and react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,180 +1,184 @@
-import Header from "./Header";
-import React, { useState } from "react";
-import Keypad from "./Keypad";
-import Display from "./Display";
+import Header from './Header'
+import React, { useState } from 'react'
+import Keypad from './Keypad'
+import Display from './Display'
+import { APP_FOLDER_NAME } from './globals'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 export default function App() {
   // debugger;
-  const [currentValue, setCurrentValue] = useState("0");
-  const [previousValue, setPreviousValue] = useState("");
-  const [isOperatorClicked, setIsOperatorClicked] = useState("");
-  const [currentOperator, setCurrentOperator] = useState("");
-  const [memoryValue, setMemoryValue] = useState("");
-  const [isEqualClicked, setIsEqualClicked] = useState("");
+  const [currentValue, setCurrentValue] = useState('0')
+  const [previousValue, setPreviousValue] = useState('')
+  const [isOperatorClicked, setIsOperatorClicked] = useState('')
+  const [currentOperator, setCurrentOperator] = useState('')
+  const [memoryValue, setMemoryValue] = useState('')
+  const [isEqualClicked, setIsEqualClicked] = useState('')
 
   function handleButtonClick(button) {
     // debugger;
-    console.log("Button value: ", button.value);
+    console.log('Button value: ', button.value)
     switch (button.type) {
-      case "operator":
-        if (button.type === "operator") {
+      case 'operator':
+        if (button.type === 'operator') {
           if (previousValue && currentOperator) {
             const operatorResult = calculate(
               previousValue,
               currentOperator,
               currentValue
-            );
-            setPreviousValue(operatorResult.toString());
-            setCurrentValue("0");
+            )
+            setPreviousValue(operatorResult.toString())
+            setCurrentValue('0')
           }
-          setIsOperatorClicked(true);
-          setCurrentOperator(button.value);
+          setIsOperatorClicked(true)
+          setCurrentOperator(button.value)
         } else {
-          setIsOperatorClicked(false);
+          setIsOperatorClicked(false)
         }
-        break;
+        break
 
-      case "enter":
+      case 'enter':
         // if (button.type === "enter" && previousValue && currentOperator) {
-        if (button.type === "enter" && previousValue && currentOperator) {
+        if (button.type === 'enter' && previousValue && currentOperator) {
           // if (button.type === "enter") {
           const enterResult = calculate(
             previousValue,
             currentOperator,
             currentValue
-          );
-          setPreviousValue(enterResult.toString());
+          )
+          setPreviousValue(enterResult.toString())
 
-          if (!isOperatorClicked && currentValue !== "") {
-            setIsEqualClicked(true);
-            setCurrentValue(enterResult.toString());
+          if (!isOperatorClicked && currentValue !== '') {
+            setIsEqualClicked(true)
+            setCurrentValue(enterResult.toString())
           } else {
-            setIsEqualClicked(false);
-            setCurrentValue("0");
+            setIsEqualClicked(false)
+            setCurrentValue('0')
           }
-          setIsOperatorClicked(false);
-          setCurrentOperator("");
+          setIsOperatorClicked(false)
+          setCurrentOperator('')
           // }
         }
-        break;
+        break
 
-      case "number":
-        if (currentValue === "0" && !isOperatorClicked) {
-          setCurrentValue(button.text.toString());
+      case 'number':
+        if (currentValue === '0' && !isOperatorClicked) {
+          setCurrentValue(button.text.toString())
         } else if (isOperatorClicked) {
-          setIsOperatorClicked(false);
-          setPreviousValue(currentValue);
-          setCurrentValue(button.text.toString());
+          setIsOperatorClicked(false)
+          setPreviousValue(currentValue)
+          setCurrentValue(button.text.toString())
         } else if (!isOperatorClicked && isEqualClicked) {
-          setCurrentValue(button.text.toString());
-          setIsEqualClicked(false);
+          setCurrentValue(button.text.toString())
+          setIsEqualClicked(false)
         } else {
           setCurrentValue(
             (prevDisplayValue) =>
               prevDisplayValue.toString() + button.text.toString()
-          );
+          )
         }
-        break;
+        break
 
-      case "clear":
-        if (button.text === "AC") {
-          setCurrentValue("0");
-          setPreviousValue("");
-          setIsOperatorClicked(false);
-          setCurrentOperator("");
-        } else if (button.text === "C") {
+      case 'clear':
+        if (button.text === 'AC') {
+          setCurrentValue('0')
+          setPreviousValue('')
+          setIsOperatorClicked(false)
+          setCurrentOperator('')
+        } else if (button.text === 'C') {
           if (button.text <= 9) {
-            setCurrentValue("0");
+            setCurrentValue('0')
           } else {
             setCurrentValue((prevDisplayValue) => {
               // console.log("PrevDisplayValue: ", prevDisplayValue);
               return (
-                prevDisplayValue.slice(0, prevDisplayValue.length - 1) || "0"
-              );
+                prevDisplayValue.slice(0, prevDisplayValue.length - 1) || '0'
+              )
               // return "10";
-            });
+            })
           }
         }
-        break;
+        break
 
-      case "memory":
-        if (button.value === "Memory Save") {
-          setMemoryValue(currentValue);
-          setCurrentValue(memoryValue.toString()); // set current value to memoryValue
-        } else if (button.value === "Memory Recall") {
-          setCurrentValue(memoryValue);
-        } else if (button.value === "Memory Clear") {
-          setMemoryValue("");
-        } else if (button.value === "Memory Addition") {
-          const mPlusResult = Number(currentValue) + Number(memoryValue);
-          setCurrentValue(mPlusResult.toString());
-        } else if (button.value === "Memory Subtract") {
-          const mMinusResult = Number(currentValue) - Number(memoryValue);
-          setCurrentValue(mMinusResult.toString());
+      case 'memory':
+        if (button.value === 'Memory Save') {
+          setMemoryValue(currentValue)
+          setCurrentValue(memoryValue.toString()) // set current value to memoryValue
+        } else if (button.value === 'Memory Recall') {
+          setCurrentValue(memoryValue)
+        } else if (button.value === 'Memory Clear') {
+          setMemoryValue('')
+        } else if (button.value === 'Memory Addition') {
+          const mPlusResult = Number(currentValue) + Number(memoryValue)
+          setCurrentValue(mPlusResult.toString())
+        } else if (button.value === 'Memory Subtract') {
+          const mMinusResult = Number(currentValue) - Number(memoryValue)
+          setCurrentValue(mMinusResult.toString())
         }
-        break;
+        break
 
-      case "decimal":
-        if (!currentValue.toString().includes(".")) {
-          setCurrentValue((prevDisplayValue) => prevDisplayValue + ".");
+      case 'decimal':
+        if (!currentValue.toString().includes('.')) {
+          setCurrentValue((prevDisplayValue) => prevDisplayValue + '.')
         }
-        break;
+        break
 
-      case "sign":
+      case 'sign':
         setCurrentValue((prevDisplayValue) => {
-          const newValue = parseFloat(prevDisplayValue) * -1;
-          return newValue.toString();
-        });
-        break;
+          const newValue = parseFloat(prevDisplayValue) * -1
+          return newValue.toString()
+        })
+        break
 
-      case "percent":
-        if (button.value === "Percent" && currentValue !== "") {
-          console.log("condition is met for percent)");
-          const percentResult = Number(currentValue) / 100;
-          setCurrentValue(percentResult.toString());
-          return percentResult;
+      case 'percent':
+        if (button.value === 'Percent' && currentValue !== '') {
+          console.log('condition is met for percent)')
+          const percentResult = Number(currentValue) / 100
+          setCurrentValue(percentResult.toString())
+          return percentResult
         }
-        break;
+        break
 
-      case "sqrt":
-        if (button.value === "Square Root" && currentValue !== "") {
-          console.log("entering the square root case)");
-          const SqrtResult = Math.sqrt(Number(currentValue));
-          setCurrentValue(SqrtResult.toString());
+      case 'sqrt':
+        if (button.value === 'Square Root' && currentValue !== '') {
+          console.log('entering the square root case)')
+          const SqrtResult = Math.sqrt(Number(currentValue))
+          setCurrentValue(SqrtResult.toString())
         }
-        break;
+        break
     }
   }
 
   function calculate(previousValue, currentOperator, currentValue) {
     switch (currentOperator) {
-      case "Add":
-        return parseFloat(previousValue) + parseFloat(currentValue);
-      case "Subtract":
-        return parseFloat(previousValue) - parseFloat(currentValue);
-      case "Multiply":
-        return parseFloat(previousValue) * parseFloat(currentValue);
-      case "Divide":
-        return parseFloat(previousValue) / parseFloat(currentValue);
+      case 'Add':
+        return parseFloat(previousValue) + parseFloat(currentValue)
+      case 'Subtract':
+        return parseFloat(previousValue) - parseFloat(currentValue)
+      case 'Multiply':
+        return parseFloat(previousValue) * parseFloat(currentValue)
+      case 'Divide':
+        return parseFloat(previousValue) / parseFloat(currentValue)
       default:
-        return "";
+        return ''
     }
   }
 
   return (
-    <>
-      <Header title="Reactulator" />
-      <div className="wrapper">
-        <Display value={currentValue} />
-        <Keypad onButtonClick={handleButtonClick} />
-      </div>
+    <Router basename={APP_FOLDER_NAME}>
+      <>
+        <Header title='Reactulator' />
+        <div className='wrapper'>
+          <Display value={currentValue} />
+          <Keypad onButtonClick={handleButtonClick} />
+        </div>
 
-      <p>current Value : {currentValue}</p>
-      <p>previous Value : {previousValue}</p>
-      <p>current operaator : {currentOperator}</p>
-      <p>Memory value : {memoryValue}</p>
-      <p>{console.log("operator", isOperatorClicked)}</p>
-      <p>{console.log("equal", isEqualClicked)}</p>
-    </>
-  );
+        <p>current Value : {currentValue}</p>
+        <p>previous Value : {previousValue}</p>
+        <p>current operaator : {currentOperator}</p>
+        <p>Memory value : {memoryValue}</p>
+        <p>{console.log('operator', isOperatorClicked)}</p>
+        <p>{console.log('equal', isEqualClicked)}</p>
+      </>
+    </Router>
+  )
 }
